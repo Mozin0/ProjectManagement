d2d893de0441e57cdbe80ab9002cad5e955f54c8
Mohammed Bilal Al-Shalan <malshalan65@gmail.com>
2024-04-16 20:32:48 +0200

    feat: data-table

---
+++ web-front/src/components/data-table/data-table.tsx
@@ -0,0 +1,153 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+  VisibilityState,
+} from "@tanstack/react-table";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Switch } from "@/components/ui/switch";
+import { useToast } from "@/components/ui/use-toast";
+import { DataTablePagination } from "./data-table-pagination";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+  floatingBar?: React.ReactNode | null
+
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+  floatingBar = null,
+}: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = useState({});
+  const [pagination] = useState({ pageIndex: 0, pageSize: 2 });
+
+  const table = useReactTable({
+    data,
+    columns,
+    
+    getCoreRowModel: getCoreRowModel(),
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    manualPagination: false,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+      pagination, 
+    },
+  });
+
+  const { toast } = useToast();
+  console.log("Can go to next page:", table.getCanNextPage());
+  console.log("Pagination state:", table.getState().pagination);
+
+  return (
+    <div className="w-full max-w-screen-md mx-auto mt-40">
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Search Project..."
+          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("name")?.setFilterValue(event.target.value)
+          }
+          className="rounded-[12px] w-auto mr-4"
+        ></Input>
+        <Button
+          variant="secondary"
+          className="w-auto mr-4 rounded-[12px]"
+          onClick={() =>
+            toast({
+              title: "Added {Name of Project Here} to Projects",
+              description: "Your project has been created successfully!",
+              duration: 3000,
+            })
+          }
+        >
+          Create New Project
+        </Button>
+        <Switch />
+      </div>
+
+      <div className="rounded-[12px] border ">
+        <Table className="table-auto">
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id} className="w-auto">
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id} className="w-auto">
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="text-center">
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+
+        <div className="flex flex-col gap-2.5">
+        <DataTablePagination table={table} />
+        {table.getFilteredSelectedRowModel().rows.length > 0 && floatingBar}
+      </div>
+      </div>
+    </div>
+  );
+}

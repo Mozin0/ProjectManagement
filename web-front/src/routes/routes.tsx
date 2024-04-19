ea78b12d86935dc595c933b5f225272abb21faa2
Mohammed Bilal Al-Shalan <malshalan65@gmail.com>
2024-04-19 23:58:50 +0200

    feat: add dynamic routing

--- web-front/src/routes/routes.tsx
+++ web-front/src/routes/routes.tsx
@@ -1,19 +1,28 @@
+import TopBar from "@/components/topBar";
 import { Toaster } from "@/components/ui/toaster";
 import About from "@/pages/About";
-import Home from "@/pages/Home"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "@/pages/Home";
+import { Component } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const AppRoutes = () => {
-    return(
-        <Router>
-            <Toaster />
-            <Routes>
-                <Route path="/" element= {<Home />}/> 
-                <Route path="/about" element= {<About />}/> 
 
-            </Routes>
-        </Router>
-    )
-}
+const routes = [
+  {path: '/' , component: Home},
+  {path: '/about' , component: About},
+]
+
+  return (
+    <Router>
+      <Toaster />
+      <TopBar />
+      <Routes>
+      {routes.map(({ path, component: Component }, index) => (
+            <Route key={index} path={path} element={<Component />} />
+          ))}
+      </Routes>
+    </Router>
+  );
+};
 
 export default AppRoutes;

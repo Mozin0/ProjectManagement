0ff5f74209eb023508aa7de5c31704b77259122d
Mohammed Bilal Al-Shalan <malshalan65@gmail.com>
2024-04-22 00:36:00 +0200

    feat: add task model

--- web-front/src/models/task.ts
+++ web-front/src/models/task.ts
@@ -1,3 +1,43 @@
+import { Guid } from "guid-typescript";
+import { Status } from "./status";
+import { Project } from "./project";
+
 export class Task {
-    
+  id: Guid;
+  name: string;
+  createdDate: Date;
+  deadline: Date;
+  parentTaskId?: Guid;
+  parentTask?: Task;
+  subTasks: Task[] = [];
+  projectId: Guid;
+  project?: Project;
+  statusId?: number;
+  status?: Status;
+
+  constructor(
+    id: Guid,
+    name: string,
+    createdDate: Date,
+    deadline: Date,
+    parentTaskId: Guid,
+    parentTask: Task,
+    subTasks: Task[],
+    projectId: Guid,
+    project: Project,
+    statusId: number,
+    status: Status
+  ) {
+    this.id = id;
+    this.name = name;
+    this.createdDate = createdDate;
+    this.deadline = deadline;
+    this.parentTaskId = parentTaskId;
+    this.parentTask = parentTask;
+    this.projectId = projectId;
+    this.project = project;
+    this.subTasks = subTasks;
+    this.statusId = statusId;
+    this.status = status;
+  }
 }

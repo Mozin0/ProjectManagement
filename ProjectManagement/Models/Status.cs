005288d3c735799dfd8f31a0057e43da1bbd554a
Mohammed Bilal Al-Shalan <malshalan65@gmail.com>
2024-04-22 00:28:20 +0200

    refactor: status class syntax

--- ProjectManagement/Models/Status.cs
+++ ProjectManagement/Models/Status.cs
@@ -1,11 +1,9 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace ProjectManagement.Models
+namespace ProjectManagement.Models;
+public class Status
 {
-    public class Status
-    {
-        public int Id { get; set; }
-        [Required]
-        public string Name { get; set; } = string.Empty;
-    }
+    public int? Id { get; set; }
+    [Required]
+    public string Name { get; set; } = string.Empty;
 }

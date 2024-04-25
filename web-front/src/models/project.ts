93bcb1077937a622d63809634aae43ce9d159d7d
Mohammed Bilal Al-Shalan <malshalan65@gmail.com>
2024-04-26 00:20:40 +0200

    refactor: update prop types and make some props nullable in project component

--- web-front/src/models/project.ts
+++ web-front/src/models/project.ts
@@ -1,19 +1,20 @@
-import {Guid} from 'guid-typescript'
 import { Task } from './task';
 import { Status } from './status';
 
 export class Project {
-    id: Guid;
+    id: string;
     name: string;
-    createdDate: Date;
-    tasks:  Task[] = [];
+    createdDate: Date = new Date();
+    deadline: string;
+    tasks?:  Task[] = [];
     statusId?: number;
     status?: Status;
 
-    constructor(id: Guid,  name: string,  createdDate: Date, tasks: Task[], statusId: number, status: Status) {
+    constructor(id: string,  name: string,  createdDate: Date, statusId: number, deadline: string, tasks?: Task[], status?: Status) {
         this.id = id;
         this.name = name;
         this.createdDate = createdDate;
+        this.deadline = deadline;
         this.tasks = tasks;
         this.statusId = statusId;
         this.status = status;

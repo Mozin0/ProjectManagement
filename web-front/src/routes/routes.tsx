d380f35faecfeabddd3c48d4185b713722b7d917
Mohammed Bilal Al-Shalan <malshalan65@gmail.com>
2024-04-28 23:41:29 +0200

    feat: implement routing for various pages

--- web-front/src/routes/routes.tsx
+++ web-front/src/routes/routes.tsx
@@ -2,24 +2,27 @@ import TopBar from "@/components/topBar";
 import { Toaster } from "@/components/ui/toaster";
 import About from "@/pages/About";
 import Home from "@/pages/Home";
-import { Component } from "react";
+import Task from "@/pages/Tasks";
+import { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const AppRoutes = () => {
-
-const routes = [
-  {path: '/' , component: Home},
-  {path: '/about' , component: About},
-]
+  const routes = [
+    { path: "/", component: Home },
+    { path: "/about", component: About },
+    { path: "/:id/tasks", component: Task },
+  ];
 
   return (
     <Router>
       <Toaster />
       <TopBar />
+      <Suspense fallback={"Loading..."} />
       <Routes>
-      {routes.map(({ path, component: Component }, index) => (
-            <Route key={index} path={path} element={<Component />} />
-          ))}
+        {routes.map(({ path, component: Component }, index) => (
+          <Route key={index} path={path} element={<Component />} />
+        ))}
+        <Route path="*" element={<div>404 - Not Found</div>} />
       </Routes>
     </Router>
   );

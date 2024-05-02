653ab12b0619a981db6be77d54430cf5f8dac2ef
Mohammed Bilal Al-Shalan <malshalan65@gmail.com>
2024-05-02 22:49:40 +0200

    feat: add fetchStatusById to StatusAPI for retrieving status details

---
+++ web-front/src/api/statusApi.ts
@@ -0,0 +1,15 @@
+import { Status } from "@/models/status";
+
+const baseurl = "http://localhost:5159/api/v1";
+
+export async function fetchStatusById(id: number) : Promise<Status> {
+    try {
+      const response = await fetch(baseurl + "/statuses/" + id);
+      if (!response.ok) throw new Error("HTTP error " + response.status);
+      const data = await response.json();
+      return data
+    } catch (error) {
+      console.error("Error Fetching Status:", error);
+      throw error;
+    }
+  }

c9817d7170955fc1e1798fc6188aa4eedfae4b97
Mohammed Bilal Al-Shalan <malshalan65@gmail.com>
2024-05-03 23:46:17 +0200

    refactor: enhance Project model with defaults, new Deadline property, and JSON adjustments

--- ProjectManagement/Models/Project.cs
+++ ProjectManagement/Models/Project.cs
@@ -1,15 +1,16 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
-namespace ProjectManagement.Models
+namespace ProjectManagement.Models;
+public class Project
 {
-    public class Project
-    {
-        public Guid Id { get; set; }
-        [Required(AllowEmptyStrings = false ,ErrorMessage = "Project name is required")]
-        public string Name { get; set; } = string.Empty;
-        public DateTime CreatedDate { get; set; }
-        public List<ProjectTask>? Tasks { get; set; } = [];
-        public int? StatusId {get; set; } 
-        public Status? Status { get; set; }
-    }
+    public Guid Id { get; set; } = Guid.Empty;
+    [Required(AllowEmptyStrings = false ,ErrorMessage = "Project name is required")]
+    public string Name { get; set; } = string.Empty;
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
+    public DateTimeOffset Deadline { get; set; }
+    public List<ProjectTask>? Tasks { get; set; } = [];
+    public int? StatusId {get; set; } 
+    [JsonIgnore]
+    public Status? Status { get; set; }
 }

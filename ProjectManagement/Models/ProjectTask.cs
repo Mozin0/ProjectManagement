f9051a8aba3860c162504a52e4af60e71c1d2b00
Mohammed Bilal Al-Shalan <malshalan65@gmail.com>
2024-05-10 23:57:32 +0200

    feat: update ProjectTask with Deadline, defaults, and JsonIgnore attributes

--- ProjectManagement/Models/ProjectTask.cs
+++ ProjectManagement/Models/ProjectTask.cs
@@ -1,22 +1,25 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
-namespace ProjectManagement.Models
+namespace ProjectManagement.Models;
+public class ProjectTask
 {
-    public class ProjectTask
-    {
-        public Guid Id { get; set; }
-        [Required(AllowEmptyStrings = false ,ErrorMessage = "Task name is required")]
-        public string Name { get; set; } = string.Empty;
-        public DateTime CreatedDate { get; set; }
+    public Guid Id { get; set; } = Guid.Empty;
+    [Required(AllowEmptyStrings = false ,ErrorMessage = "Task name is required")]
+    public string Name { get; set; } = string.Empty;
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
+    public DateTime Deadline { get; set; }
 
-        // Sub Tasks
-        public Guid? ParentTaskId { get; set; }
-        public ProjectTask? ParentTask { get; set; } 
-        public List<ProjectTask>? SubTasks { get; set; } = [];
+    // Sub Tasks
+    public Guid? ParentTaskId { get; set; } = Guid.Empty;
+    [JsonIgnore]
+    public ProjectTask? ParentTask { get; set; } 
+    public List<ProjectTask>? SubTasks { get; set; } = [];
 
-        public Guid ProjectID { get; set; }
-        public Project? Project { get; set; }
-        public int? StatusId {get; set; }
-        public Status? Status { get; set; }
-    }
+    public Guid ProjectID { get; set; } = Guid.Empty;
+    [JsonIgnore]
+    public Project? Project { get; set; }
+    public int? StatusId {get; set; }
+    [JsonIgnore]
+    public Status? Status { get; set; }
 }

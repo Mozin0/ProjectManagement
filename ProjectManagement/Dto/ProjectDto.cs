c1470aea88842acca1ee1f9d05fdfac42bdfd018
Mohammed Bilal Al-Shalan <malshalan65@gmail.com>
2024-05-11 23:28:45 +0200

    feat: update ProjectDto with Deadline, defaults, and JsonIgnore attributes

--- ProjectManagement/Dto/ProjectDto.cs
+++ ProjectManagement/Dto/ProjectDto.cs
@@ -1,15 +1,17 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
-namespace ProjectManagement.Dto
+namespace ProjectManagement.Dto;
+public class ProjectDto
 {
-    public class ProjectDto
-    {
-        public Guid Id { get; set; }
-        [Required]
-        public string Name { get; set; } = string.Empty;
-        public DateTime CreatedDate { get; set; }
-        public List<ProjectTaskDto>? Tasks { get; set; } = [];
-        public int? StatusId {get; set; }
-        public StatusDto? Status { get; set; }
-    }
+    public Guid Id { get; set; } = Guid.Empty;
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
+    public DateTimeOffset Deadline { get; set; }
+
+    public List<ProjectTaskDto>? Tasks { get; set; } = [];
+    public int? StatusId {get; set; }
+    [JsonIgnore]
+    public StatusDto? Status { get; set; }
 }

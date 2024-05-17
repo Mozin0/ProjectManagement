c8c0597762d230bcd20bcbfe57ede77bb8ab6c0a
Mohammed Bilal Al-Shalan <malshalan65@gmail.com>
2024-05-17 23:52:49 +0200

    feat: add fetchStatuses to statusApi

--- web-front/src/api/statusApi.ts
+++ web-front/src/api/statusApi.ts
@@ -2,6 +2,18 @@ import { Status } from "@/models/status";
 
 const baseurl = "http://localhost:5159/api/v1";
 
+export async function fetchStatuses(): Promise<Status[]> {
+  try {
+    const response = await fetch(baseurl + "/statuses");
+    if (!response.ok) throw new Error("HTTP error " + response.status);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching statuses:", error);
+    throw error;
+  }
+}
+
 export async function fetchStatusById(id: number) : Promise<Status> {
     try {
       const response = await fetch(baseurl + "/statuses/" + id);

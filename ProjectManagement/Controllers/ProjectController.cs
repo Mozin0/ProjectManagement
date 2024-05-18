edca7e42145c6ddedd4c0d3a8363f38b209b95ce
Mohammed Bilal Al-Shalan <malshalan65@gmail.com>
2024-05-18 23:23:15 +0200

    feat: remove logger for projectController

--- ProjectManagement/Controllers/ProjectController.cs
+++ ProjectManagement/Controllers/ProjectController.cs
@@ -2,63 +2,60 @@ using Microsoft.AspNetCore.Mvc;
 using ProjectManagement.Dto;
 using ProjectManagement.Managers;
 
-namespace ProjectManagement.Controllers
+namespace ProjectManagement.Controllers;
+[ApiController]
+[Route("api/v1/projects")]
+public class ProjectController(ProjectManager manager) : ControllerBase
 {
-    [ApiController]
-    [Route("api/v1/projects")]
-    public class ProjectController(ProjectManager manager) : ControllerBase
+    private readonly ProjectManager _manager = manager;
+    [HttpGet]
+    public IActionResult Get()
     {
-        private readonly ProjectManager _manager = manager;
-
-        [HttpGet]
-        public IActionResult Get()
+        var projects = _manager.GetProjectsList();
+        if (projects == null)
         {
-            var projects = _manager.GetProjectsList();
-            if (projects == null)
-            {
-                return NotFound(projects);  
-            }
-            return Ok(projects);
+            return NotFound(projects);  
         }
+        return Ok(projects);
+    }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetByIdAsync(Guid id)
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetByIdAsync(Guid id)
+    {
+        var project = await _manager.GetProjectByIdAsync(id);
+        if (project == null)
         {
-            var project = await _manager.GetProjectByIdAsync(id);
-            if (project == null)
-            {
-                return NotFound(project);  
-            }
-            return Ok(project);
+            return NotFound(project);  
         }
+        return Ok(project);
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> PostAsync(ProjectDto projectDto)
+    [HttpPost]
+    public async Task<IActionResult> PostAsync(ProjectDto projectDto)
+    {
+        var project = await _manager.PostProjectAsync(projectDto);
+        if (project == null)
         {
-            var project = await _manager.PostProjectAsync(projectDto);
-            if (project == null)
-            {
-                return NotFound(project);  
-            }
-            return Created(string.Empty, project);
+            return NotFound(project);  
         }
+        return Created(string.Empty, project);
+    }
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutAsync(Guid id, ProjectDto project)
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAsync(Guid id, ProjectDto project)
+    {
+        var projectObj = await _manager.PutProjectAsync(id, project);
+        if (projectObj == null)
         {
-            var projectObj = await _manager.PutProjectAsync(id, project);
-            if (projectObj == null)
-            {
-                return NotFound(projectObj);  
-            }
-            return Ok(projectObj);
+            return NotFound(projectObj);  
         }
+        return Ok(projectObj);
+    }
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteAsync(Guid id)
-        {
-            await _manager.DeleteProjectAsync(id);
-            return Ok();
-        }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(Guid id)
+    {
+        await _manager.DeleteProjectAsync(id);
+        return Ok();
     }
 }

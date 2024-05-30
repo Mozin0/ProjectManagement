4bb7a3d7101cbbbb3a934b843be202f051b0fd52
Mohammed Bilal Al-Shalan <malshalan65@gmail.com>
2024-05-30 23:55:17 +0200

    feat: implement onMoelCreating method target Project an Task ID

--- ProjectManagement/DbContexts/ProjectManagementDbContext.cs
+++ ProjectManagement/DbContexts/ProjectManagementDbContext.cs
@@ -1,12 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using ProjectManagement.Models;
 
-namespace ProjectManagement.DbContexts
-{
-    public class ProjectManagementDbContext(DbContextOptions<ProjectManagementDbContext> options) : DbContext(options)
-    {  
-        public DbSet<Status> Statuses { get; set; }
-        public DbSet<ProjectTask> Tasks { get; set; }
-        public DbSet<Project> Projects { get; set; }
+namespace ProjectManagement.DbContexts;
+public class ProjectManagementDbContext(DbContextOptions<ProjectManagementDbContext> options) : DbContext(options)
+{  
+    public DbSet<Status> Statuses { get; set; }
+    public DbSet<ProjectTask> Tasks { get; set; }
+    public DbSet<Project> Projects { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuidler) 
+    {
+        modelBuidler.Entity<Project>()
+            .Property(p => p.Id)
+            .ValueGeneratedOnAdd();
+
+        modelBuidler.Entity<ProjectTask>()
+            .Property(p => p.Id)
+            .ValueGeneratedOnAdd();    
     }
 }
